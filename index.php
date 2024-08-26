0dea45c0591e259f8bdbc53e9746a7e3a7ef4911
angembang <132391052+angembang@users.noreply.github.com>
2024-08-26 22:15:09 +0200

    chore: Initialize application environment and handle CSRF token
    - Started a new session and loaded the Composer autoloader.
    - Loaded environment variables from the .env file using Dotenv.
    - Checked if the CSRF token is not set in the session; if not, generated and stored a new CSRF token using CSRFTokenManager.
    - Initialized an instance of the Router class.
    - Routed the request based on $_GET parameters.
    
    - Ensures secure request handling and application environment setup.

--- index.php
+++ index.php
@@ -1,2 +1,38 @@
-<?php 
+<?php
+// Start the session
+session_start();
+
+
+// Load the Composer autoloader
+require "vendor/autoload.php";
+
+// Load the contents of the .env file into $_ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+// Check if the CSRF token is not set in the session
+if(!isset($_SESSION["csrf-token"]))
+{
+    // Generate a CSRF token
+    $tokenManager = new CSRFTokenManager();
+    $token = $tokenManager->generateCSRFToken();
+
+    // Store the CSRF token in the session
+    $_SESSION["csrf-token"] = $token;
+}
+
+
+/**
+ * @var Router $router an instance of the Router class for handling requests.
+ */
+$router = new Router();
+
+/**
+ * Handle the request based on the $_GET parameters.
+ */
+$router->handleRequest($_GET);
+
+
+
 echo "Bienvenu sur le site de l'association Renais'sance";
