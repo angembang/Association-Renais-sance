9e8bb8ccfa2b7e6d319f7be6ccf109e9d631fcd2
angembang <132391052+angembang@users.noreply.github.com>
2024-08-26 22:00:02 +0200

    feat: Add AbstractManager class for database handling
    - Implemented AbstractManager class to provide a base for database interactions.
      - Includes a protected PDO instance for database connection.
      - Constructor initializes the PDO connection using environment variables for host, charset, database name, user, and password.
    
    - Provides a common foundation for other manager classes to handle database operations.

---
+++ managers/AbstractManager.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Class AbstractManager
+ * Abstract class to provide database connection and common methods for other managers.
+ */
+abstract class AbstractManager 
+{
+  /**
+   *  @var PDO the PDO instance for database connection.
+   */
+  protected PDO $db;
+
+  /**
+   * AbstractManager constructor
+   * Initialize the database connection using the provided environment variables.
+   */ 
+  public function __construct()
+  {
+    // Construct the PDO connection string using environment variables.
+    $connexion = "mysql:host=".$_ENV["DB_HOST"].";port=3306;charset=".$_ENV["DB_CHARSET"]."; dbname=".$_ENV["DB_NAME"];
+
+    // Initialize the PDO instance for database connection.
+    $this->db = new PDO(
+      $connexion,
+      $_ENV["DB_USER"],
+      $_ENV["DB_PASSWORD"]
+    );
+  } 
+  
+}
