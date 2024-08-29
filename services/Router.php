855653a4607dfe15d368981d33f99aa2f656a649
angembang <132391052+angembang@users.noreply.github.com>
2024-08-29 23:26:20 +0200

    feat(router): implement basic routing structure with initial authentication and page routes
    - Added a Router class to handle routing based on  parameters.
    - Included initial routes for registration, login, logout, and home.
    - The class is designed to be easily extended with additional routes as the site develops.

---
+++ services/Router.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Class Router
+ * Handles the routing of requests.
+ */
+class Router
+{
+  /**
+   * Handles the incoming request based on the provided $_GET parameters.
+   *
+   * @param array $get The associative array of $_GET parameters.
+   * 
+   * @return void
+   */
+  public function handleRequest(array $get): void
+  {
+    // Instantiate the necessary controllers
+    $authController = new AuthController();
+    $pageController = new PageController();
+        
+    // Check if a route is provided
+    if(isset($get["route"])) {
+      // Switch statement for routing
+      switch($get["route"]) {
+        case "inscription":
+          $authController->register(); 
+          break;
+                    
+        case "checkRegister":
+          $authController->checkRegister();
+          break;
+                
+        case "connexion":
+          $authController->login();
+          break;
+                    
+        case "checkLogin":
+          $authController->checkLogin(); 
+          break;
+                    
+        case "logout":
+          $authController->logout();    
+          break;
+                    
+        default:
+          $pageController->home(); 
+          break;
+      } 
+            
+    } else {
+      // Route is not provided/ render the home page
+      $pageController->home();
+      die;
+      } 
+  }
+        
+}
