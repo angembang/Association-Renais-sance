f8826ade5d3476fb55385f9efab37dcec71a42fe
angembang <132391052+angembang@users.noreply.github.com>
2024-09-20 10:50:47 +0200

    Feat: integrated TinyMCE text editor for content fields in news and event forms
    Added tinyMCE-config.js to enable rich text editing in the add news and add event forms.

---
+++ assets/js/tinyMCE-config.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  tinymce.init({
+    selector: '#contenu',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+  });
+});
